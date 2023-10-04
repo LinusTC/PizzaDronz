cb4054c601b22a5bc8b897fa5e28b3920abd0609
LinusTC <tongchun03@gmail.com>
2023-10-04 11:44:04 +0100

    Files

---
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -0,0 +1,27 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.data.LngLat;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
+import uk.ac.ed.inf.ilp.interfaces.LngLatHandling;
+
+public class LngLatHandler implements LngLatHandling{
+    @Override
+    public double distanceTo(LngLat startPosition, LngLat endPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
+        return false;
+    }
+
+    @Override
+    public boolean isInRegion(LngLat position, NamedRegion region) {
+        return false;
+    }
+
+    @Override
+    public LngLat nextPosition(LngLat startPosition, double angle) {
+        return null;
+    }
+}
--- src/main/java/uk/ac/ed/inf/LngLatHandling.java
+++
@@ -1,4 +0,0 @@
-package uk.ac.ed.inf;
-
-public class LngLatHandling {
-}
