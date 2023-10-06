98dddd8608753541dedaf3cff796c8c3f5b47795
LinusTC <tongchun03@gmail.com>
2023-10-06 23:51:27 +0100

    Files

--- src/main/java/uk/ac/ed/inf/LngLatHandler.java
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -7,12 +7,21 @@ import uk.ac.ed.inf.ilp.interfaces.LngLatHandling;
 public class LngLatHandler implements LngLatHandling{
     @Override
     public double distanceTo(LngLat startPosition, LngLat endPosition) {
-        return 0;
+        double x1 = startPosition.lng();
+        double y1 = startPosition.lat();
+
+        double x2 = endPosition.lng();
+        double y2 = endPosition.lat();
+
+        double x = (x1-x2);
+        double y = (y1-y2);
+
+        return Math.sqrt(x*x + y*y);
     }
 
     @Override
     public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
-        return false;
+        return (distanceTo(startPosition, otherPosition) < 0.00015);
     }
 
     @Override
