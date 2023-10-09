09a8e99f9a9e304ad9c2e1ce911687875747f185
LinusTC <tongchun03@gmail.com>
2023-10-09 20:57:12 +0100

    Files

--- src/test/java/uk/ac/ed/inf/LngLatHandlerTests.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.ed.inf;
-import junit.framework.TestCase;
-import uk.ac.ed.inf.LngLatHandler;
-public class LngLatHandlerTests extends TestCase {
-}
--- src/test/java/uk/ac/ed/inf/OrderValidatorTests.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ed.inf;
-import junit.framework.TestCase;
-import uk.ac.ed.inf.LngLatHandler;
-
-public class OrderValidatorTests extends TestCase {
-
-}
