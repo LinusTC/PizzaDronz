434d3bcfc7ea1348e1a3ae8705972b12c5408dc8
LinusTC <tongchun03@gmail.com>
2023-11-04 06:05:25 +0000

    mhm

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,8 +1,46 @@
 package uk.ac.ed.inf;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class App
 {
-    public static void main( String[] args ){
+    public static void main(String[] args){
 
+        String date = args[0];
+        String url = args[1];
+
+        if(!validURL(url)){
+            System.out.println("Invalid URL. Use https://ilp-rest.azurewebsites.net.");
+            System.exit(0);
+        }
+
+        if (!validDate(date)){
+            System.out.println("Invalid date format. Please use the format 'yyyy-MM-dd'.");
+            System.exit(0);
+        }
+
+        else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate localDate = LocalDate.parse(date, formatter);
+
+            System.out.println(localDate);
+            System.out.println(url);
+
+        }
+
+    }
+    public static boolean validDate (String date){
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate.parse(date, formatter);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean validURL (String URL){
+        return URL.equals("https://ilp-rest.azurewebsites.net");
     }
 }
