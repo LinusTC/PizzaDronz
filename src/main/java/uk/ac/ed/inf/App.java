ee20918bbe7319b9694af797479437d18478b5ad
LinusTC <tongchun03@gmail.com>
2023-11-10 03:43:07 +0000

    mhm

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -14,7 +14,7 @@ public class App
 //        String dateInput = args[0];
 //        String urlInput = args[1];
 
-        String dateInput = "2023-09-07";
+        String dateInput = "2023-09-01";
         String urlInput = "https://ilp-rest.azurewebsites.net";
 
         if (!validDate(dateInput)){
